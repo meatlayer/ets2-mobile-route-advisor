9ae4be9f2992093fc321f97ecf0e174c5f6456e9
Dmitriy <termid@list.ru>
2022-07-28 03:02:03 +0300

    v4.0.3

--- dashboard.js
+++ dashboard.js
@@ -622,7 +622,7 @@ var g_translations;
 var g_skinConfig;
 
 // The current version of ets2-mobile-route-advisor
-var g_currentVersion = '4.0.2';
+var g_currentVersion = '4.0.3';
 
 // The currently running game
 var g_runningGame;
